5d3edb5b833cb4af5e1b6363a4f64e499599cdda
balaclava-guy <blclvgy@proton.me>
2026-02-22 06:44:21 +0000

    Paramaterize update actions and modes
    Added new update modes and actions for it8951e display.

--- components/it8951e/display.py
+++ components/it8951e/display.py
@@ -10,7 +10,6 @@ from esphome.const import (
     CONF_RESET_PIN,
     CONF_RESET_DURATION,
     CONF_BUSY_PIN,
-    CONF_PAGES,
     CONF_LAMBDA,
     CONF_MODEL,
     CONF_REVERSED,
@@ -33,16 +32,42 @@ ClearAction = it8951e_ns.class_("ClearAction", automation.Action)
 UpdateSlowAction = it8951e_ns.class_("UpdateSlowAction", automation.Action)
 DrawAction = it8951e_ns.class_("DrawAction", automation.Action)
 
-# New actions
-UpdateFastAction = it8951e_ns.class_("UpdateFastAction", automation.Action)
-UpdateA2Action = it8951e_ns.class_("UpdateA2Action", automation.Action)
-UpdateGL16Action = it8951e_ns.class_("UpdateGL16Action", automation.Action)
+# New: parameterized update action
+UpdateModeAction = it8951e_ns.class_("UpdateModeAction", automation.Action)
 
 it8951eModel = it8951e_ns.enum("it8951eModel")
 
 MODELS = {
     "M5EPD": it8951eModel.M5EPD,
 }
+
+# New: expose update modes as config values
+UpdateMode = it8951e_ns.enum("update_mode_e")
+
+# Modes (high-level guidance)
+# INIT  - full clean/flash erase, slowest, best for getting rid of ghosting.
+# DU    - very fast B/W update, minimal flash, good for boot splash and small changes.
+# DU4   - fast 4-level grayscale-ish, more artifacts than GL16/GC16, usually less flash than full.
+# A2    - fast B/W optimized for frequent changes (more ghosting), good for quick UI feedback.
+# GL16  - “grayscale” update (text/lines look good), slower than DU/DU4/A2.
+# GC16  - full quality grayscale update, slowest “normal” mode.
+# GLR16/GLD16 - variants used for different waveform behavior; depends on panel/LUT.
+# NONE  - no update.
+
+UPDATE_MODES = {
+    "INIT": UpdateMode.UPDATE_MODE_INIT,
+    "DU": UpdateMode.UPDATE_MODE_DU,
+    "GC16": UpdateMode.UPDATE_MODE_GC16,
+    "GL16": UpdateMode.UPDATE_MODE_GL16,
+    "GLR16": UpdateMode.UPDATE_MODE_GLR16,
+    "GLD16": UpdateMode.UPDATE_MODE_GLD16,
+    "DU4": UpdateMode.UPDATE_MODE_DU4,
+    "A2": UpdateMode.UPDATE_MODE_A2,
+    "NONE": UpdateMode.UPDATE_MODE_NONE,
+}
+
+CONF_MODE = "mode"
+CONF_DESCRIPTION = "description"
 
 CONFIG_SCHEMA = cv.All(
     display.FULL_DISPLAY_SCHEMA.extend(
@@ -93,52 +118,43 @@ CONFIG_SCHEMA = cv.All(
     ),
 )
 
-# New: fast-ish modes
+# New: one action with a mode parameter.
+# We add an optional 'description' field purely for humans; some editors show it, many won't,
+# but it never harms validation.
 @automation.register_action(
-    "it8951e.updatefast",
-    UpdateFastAction,
-    automation.maybe_simple_id(
+    "it8951e.update",
+    UpdateModeAction,
+    cv.Schema(
         {
-            cv.GenerateID(): cv.use_id(IT8951ESensor),
+            cv.Required(CONF_ID): cv.use_id(IT8951ESensor),
+            cv.Required(CONF_MODE): cv.enum(UPDATE_MODES, upper=True),
+            cv.Optional(CONF_DESCRIPTION): cv.string,
         }
     ),
 )
-@automation.register_action(
-    "it8951e.updatea2",
-    UpdateA2Action,
-    automation.maybe_simple_id(
-        {
-            cv.GenerateID(): cv.use_id(IT8951ESensor),
-        }
-    ),
-)
-@automation.register_action(
-    "it8951e.updategl16",
-    UpdateGL16Action,
-    automation.maybe_simple_id(
-        {
-            cv.GenerateID(): cv.use_id(IT8951ESensor),
-        }
-    ),
-)
+async def it8951e_update_to_code(config, action_id, template_arg, args):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    cg.add(var.set_mode(config[CONF_MODE]))
+    # 'description' is intentionally ignored at codegen time.
+    return var
+
 
 async def it8951e_clear_to_code(config, action_id, template_arg, args):
     var = cg.new_Pvariable(action_id, template_arg)
     await cg.register_parented(var, config[CONF_ID])
     return var
 
-# All of these actions are “simple id” actions so we can use the same to_code helper.
+
 async def it8951e_simple_parented_action_to_code(config, action_id, template_arg, args):
     var = cg.new_Pvariable(action_id, template_arg)
     await cg.register_parented(var, config[CONF_ID])
     return var
 
-# Bind the new actions to the shared helper
+
 it8951e_updateslow_to_code = it8951e_simple_parented_action_to_code
 it8951e_draw_to_code = it8951e_simple_parented_action_to_code
-it8951e_updatefast_to_code = it8951e_simple_parented_action_to_code
-it8951e_updatea2_to_code = it8951e_simple_parented_action_to_code
-it8951e_updategl16_to_code = it8951e_simple_parented_action_to_code
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
